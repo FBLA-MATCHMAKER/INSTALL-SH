41c750ec14e3a680a37f79db733cd92e3939cbc2
Larson Carter <larsoncarter@blazermail.net>
2019-02-11 14:25:40 -0600

    Update macos-install.sh

--- macos-install.sh
+++ macos-install.sh
@@ -1,1 +1,19 @@
 #!/bin/sh
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap caskroom/cask
+
+brew tap caskroom/versions
+
+brew cask install java
+
+brew cask install java8
+
+git clone https://github.com/FBLA-MATCHMAKER/matchmakerJava-Simplified/
+
+cd matchmakerJava-Simplified/VVFBLA-MATCHMAKER/src/
+
+javac Program.java
+
+java Program
